aa417c877810090d8a3bd4f762b95621c5ea008c
NIFr3d <fred-dls@orange.fr>
2021-11-16 17:34:12 +0100

    16/11 18h

--- Appweb2021/client.js
+++ Appweb2021/client.js
@@ -1,27 +1,37 @@
 let socket = new WebSocket("ws://localhost:81");
 var data;
+const queryString=window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const loginfos={userid:urlParams.get("login"),mdp:urlParams.get("mdp")};
 
-socket.onopen = function() {
-    alert("[open] Connection established");
-    alert("Sending to server");
-    socket.send(JSON.stringify({"userid":"fre1","mdp":"test"})); //à changer pour vérifier si l'accès est autorisé
+
+  socket.onopen = function() {
+    socket.send(JSON.stringify(loginfos));
   };
   
   socket.onmessage = function(event) {
     data = JSON.parse(event.data);
-    var datas=data.split(";");
-    for(var i=0;i<3;i++){
-      document.getElementById(i).innerHTML = datas[i];
-    };
+    if (data.event=="connexion"){
+      document.getElementById("prenom").innerHTML = data.prenom;
+      document.getElementById("temps").innerHTML = data.temps;
+      document.getElementById("vitesse").innerHTML = data.vitesse;
+      document.getElementById("conso").innerHTML = data.consommation;
+      var acces=data.acces;
+      if (acces==1) {
+        document.getElementById("adduser").style.display="block";
+      }
+      else {
+        document.getElementById("adduser").style.display="none";
+      }
+    }
   };
   
   socket.onclose = function(event) {
     if (event.wasClean) {
-      if (event.code==1005) alert("Identifiant ou mot de passe incorrect")
-      else alert(`[close] Connection closed cleanly, code=${event.code} reason=${event.reason}`);
-    } else {
-
-      alert('[close] Connection died');
+      if (event.code==4001 || event.code==4002) {
+        alert(event.reason);
+        window.location.replace("login.html");
+      }
     }
   };
   
--- Appweb2021/server.js
+++ Appweb2021/server.js
@@ -2,7 +2,6 @@ const WebSocketServer = require("ws").Server;
 const wss = new WebSocketServer( {port : 81});
 var tabl = new Set();
 const mysql = require("sync-mysql");
-var donnees="";
 
 function getsqldata(){
    result = con.query("SELECT * FROM `data` WHERE `dataid` = 1")
@@ -15,7 +14,7 @@ function getsqlusers(userid){
 
 
 
-    const con = new mysql({
+    const con = new mysql({ //à changer avec les paramètres du serveur mysql
        host: "localhost",
        user: "root",
        password: "root",
@@ -30,27 +29,29 @@ wss.on("connection", function(ws){
         catch(error){
             var obj = {err:"not json object"};
         }
-        console.dir(obj);
-        result = await getsqlusers(obj.userid);
-        if(result.length==0){
+        var result = await getsqlusers(obj.userid); //on fait une requete sql des données associées à l'identifiant entré
+        if(result.length==0){ //si le résultat de la requète est vide, l'utilisateur n'existe pas
             console.log("Utilisateur non trouvé dans la BDD");
-            ws.close();
+            ws.close(4001,"Identifiant incorrect"); //codes custom >4000, utilisés pour changer le msg de refus de connexion
             return;
         }
-        
-        if(obj.mdp==!result.mdp){ ; 
+        if(obj.mdp!=result[0].mdp){ //si mdp ne correspond pas
             console.log("Mdp incorrect");
-            ws.close()
+            ws.close(4002,"Mot de passe incorrect");
             return
         }
-        console.log("Utilisateur et mdp correct");
-
-        
-        
-        donnees=getsqldata("SELECT * FROM `data` WHERE `dataid` = 1")
-               
-        console.log(donnees);
-        ws.send(JSON.stringify(donnees));
+        console.log("Utilisateur et mdp correct"); //sinon c'est ok
+        var donnees= await getsqldata("SELECT * FROM `data` WHERE `dataid` = 1") //on recup les données depuis mysql
+        donneesenvoyees={ //données a envoyer au client pour les afficher
+            event:"connexion",
+            nom:result[0].nom,
+            prenom:result[0].prenom,
+            acces:result[0].acces,
+            temps:donnees[0].temps,
+            vitesse:donnees[0].vitesse,
+            consommation:donnees[0].consommation
+        }
+        ws.send(JSON.stringify(donneesenvoyees));
     
 
 
