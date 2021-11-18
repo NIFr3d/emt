13ce3545435e1b17785fa78fc9da6e04c28509f4
NIFr3d <fred-dls@orange.fr>
2021-11-18 11:59:48 +0100

    18/11 12h

--- Appweb2021/client.js
+++ Appweb2021/client.js
@@ -1,17 +1,25 @@
-let socket = new WebSocket("ws://localhost:81");
+let socket; 
 var data;
-const queryString=window.location.search;
-const urlParams = new URLSearchParams(queryString);
-const loginfos={userid:urlParams.get("login"),mdp:urlParams.get("mdp")};
+var loginfos;
+document.getElementById("loginbutton").onclick=function(){
+      loginfos={event:"connexionEntrant",
+                userid:document.getElementById("login").value,
+                mdp:document.getElementById("mdp").value
+              };
+      socket= new WebSocket("ws://localhost:81");
+      socket.onopen = function() {
+        socket.send(JSON.stringify(loginfos));
+      };
+    
 
 
-  socket.onopen = function() {
-    socket.send(JSON.stringify(loginfos));
-  };
+  
   
   socket.onmessage = function(event) {
     data = JSON.parse(event.data);
     if (data.event=="connexion"){
+      document.getElementById("connexion").style.display="none";
+      document.getElementById("data").style.display="block";
       document.getElementById("prenom").innerHTML = data.prenom;
       document.getElementById("temps").innerHTML = data.temps;
       document.getElementById("vitesse").innerHTML = data.vitesse;
@@ -20,9 +28,6 @@ const loginfos={userid:urlParams.get("login"),mdp:urlParams.get("mdp")};
       if (acces==1) {
         document.getElementById("adduser").style.display="block";
       }
-      else {
-        document.getElementById("adduser").style.display="none";
-      }
     }
   };
   
@@ -30,11 +35,25 @@ const loginfos={userid:urlParams.get("login"),mdp:urlParams.get("mdp")};
     if (event.wasClean) {
       if (event.code==4001 || event.code==4002) {
         alert(event.reason);
-        window.location.replace("login.html");
+        window.location.replace("main.html");
       }
     }
   };
   
   socket.onerror = function(error) {
-    alert(`[error] ${error.message}`);
+    console.dir(error)
   };
+
+document.getElementById("adduserbutton").onclick=function(){
+  toSend={
+    event:"adduser",
+    userid:document.getElementById("login1").value,
+    nom:document.getElementById("nom").value,
+    prenom:document.getElementById("prenom").value,
+    mdp:document.getElementById("mdp1").value,
+    acces:document.getElementById("acces").value,
+  }
+  
+  socket.send(JSON.stringify(toSend));
+}
+}
--- Appweb2021/server.js
+++ Appweb2021/server.js
@@ -11,6 +11,12 @@ function getsqlusers(userid){
     result = con.query("SELECT * FROM `utilisateur` WHERE `userid` = '"+userid+"'");
     return result;
  }
+ function addsqluser(json){
+     con.query("INSERT INTO `utilisateur` (`nom`, `prenom`, `mdp`, `acces`, `userid`) VALUES ('"+json.nom+"', '"+json.prenom+"', '"+json.mdp+"', '"+json.acces+"', '"+json.userid+"')");
+ }
+ function sqlquery(query){
+     con.query(query);
+ }
 
 
 
@@ -29,30 +35,36 @@ wss.on("connection", function(ws){
         catch(error){
             var obj = {err:"not json object"};
         }
-        var result = await getsqlusers(obj.userid); //on fait une requete sql des données associées à l'identifiant entré
-        if(result.length==0){ //si le résultat de la requète est vide, l'utilisateur n'existe pas
-            console.log("Utilisateur non trouvé dans la BDD");
-            ws.close(4001,"Identifiant incorrect"); //codes custom >4000, utilisés pour changer le msg de refus de connexion
-            return;
+        console.log(obj);
+        if(obj.event=="connexionEntrant"){
+            obj.userid=obj.userid.replace("`","") //on empeche les injections sql (piratage) en supprimant les caractères dangereux
+            var result = await getsqlusers(obj.userid); //on fait une requete sql des données associées à l'identifiant entré
+            if(result.length==0){ //si le résultat de la requète est vide, l'utilisateur n'existe pas
+                console.log("Utilisateur non trouvé dans la BDD");
+                ws.close(4001,"Identifiant incorrect"); //codes custom >4000, utilisés pour changer le msg de refus de connexion
+                return;
+            }
+            if(obj.mdp!=result[0].mdp){ //si mdp ne correspond pas
+                console.log("Mdp incorrect");
+                ws.close(4002,"Mot de passe incorrect");
+                return
+            }
+            console.log("Utilisateur et mdp correct"); //sinon c'est ok
+            var donnees= await getsqldata("SELECT * FROM `data` WHERE `dataid` = 1") //on recup les données depuis mysql
+            donneesenvoyees={ //données a envoyer au client pour les afficher
+                event:"connexion",
+                nom:result[0].nom,
+                prenom:result[0].prenom,
+                acces:result[0].acces,
+                temps:donnees[0].temps,
+                vitesse:donnees[0].vitesse,
+                consommation:donnees[0].consommation
+            }
+            ws.send(JSON.stringify(donneesenvoyees));
         }
-        if(obj.mdp!=result[0].mdp){ //si mdp ne correspond pas
-            console.log("Mdp incorrect");
-            ws.close(4002,"Mot de passe incorrect");
-            return
+        if(obj.event=="adduser"){
+            addsqluser(obj);
         }
-        console.log("Utilisateur et mdp correct"); //sinon c'est ok
-        var donnees= await getsqldata("SELECT * FROM `data` WHERE `dataid` = 1") //on recup les données depuis mysql
-        donneesenvoyees={ //données a envoyer au client pour les afficher
-            event:"connexion",
-            nom:result[0].nom,
-            prenom:result[0].prenom,
-            acces:result[0].acces,
-            temps:donnees[0].temps,
-            vitesse:donnees[0].vitesse,
-            consommation:donnees[0].consommation
-        }
-        ws.send(JSON.stringify(donneesenvoyees));
-    
 
 
     })
