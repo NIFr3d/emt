da69626a47e1598e2bea8c1694198646e25f1e26
LucasPrintz <lucasprintz57@gmail.com>
2021-12-07 16:17:55 +0100

    jsp

--- Appweb2021_test/scripts/map.js
+++ Appweb2021_test/scripts/map.js
@@ -1,7 +1,7 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoibHVjYXNwcmludHoiLCJhIjoiY2t3bTRncHowMHp0ODJ3cGR0ZmV0ankzbCJ9.HVm_k6XdwD4lFQBubqLeqg';
+mapbox.accessToken = 'pk.eyJ1IjoibHVjYXNwcmludHoiLCJhIjoiY2t3bTRncHowMHp0ODJ3cGR0ZmV0ankzbCJ9.HVm_k6XdwD4lFQBubqLeqg';
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
+    center: [6.9176, 48.3009], // starting position [lng, lat]
     zoom: 9 // starting zoom
     });
