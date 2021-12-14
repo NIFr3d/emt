8f02b709f6bd2c97669e5c9631fa7bab2acffa0f
LucasPrintz <lucasprintz57@gmail.com>
2021-12-14 15:36:25 +0100

    test

--- Appweb2021_test/scripts/data.js
+++ Appweb2021_test/scripts/data.js
@@ -1,19 +1,15 @@
 var socket = new WebSocket("ws://localhost:81");
 var queryString = window.location.search;
 var urlParams = new URLSearchParams(queryString);
-if(document.cookie==""){
-    location.replace("../pages/login.html");
-}
+
 
 var cookies=document.cookie.split(";");
 var userid = cookies[0].split("=")[1];
 var prenom=cookies[3].split("=")[1];
 var islogged=cookies[4].split("=")[1];
-if(islogged!=1){
-    location.replace("../pages/login.html");
-}
 
-prenom=unescape(prenom); //unescape sert a retransformer les accents
+
+prenom=decodeURI(prenom); //unescape sert a retransformer les accents
 document.getElementById("prenom").innerHTML = prenom;
 socket.onopen = function () {
   socket.onmessage = function (event) {
@@ -49,6 +45,7 @@ canevas.addEventListener('mousedown', function (e) {
             }
             else {
                 contexte.lineTo(x, y);
+                contexte.strokeStyle="#FF281F";
                 contexte.stroke();
             }
             break;
@@ -64,3 +61,25 @@ envoitracer.addEventListener('click', function (e) {
     }
     socket.send(JSON.stringify(toSend));
 });
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibHVjYXNwcmludHoiLCJhIjoiY2t3bTRncHowMHp0ODJ3cGR0ZmV0ankzbCJ9.HVm_k6XdwD4lFQBubqLeqg';
+const poscentre=[6.9176, 48.3009];
+const poscanvas;
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/light-v10', // style URL
+    center: poscentre, // starting position [lng, lat]
+    zoom: 15.8 // starting zoom
+    });
+
+for(let i=0; i<100;i++){
+    const marker = new mapboxgl.Marker();
+    setTimeout(function(){
+    marker.setLngLat([poscentre[0]+i*0.00001, poscentre[1]+i*0.00001])
+    .addTo(map);
+    poscanvas=marker.getLngLat();
+    },2000*i);
+    setTimeout(function(){
+    marker.remove();
+    },2000*(i+1))
+    };
