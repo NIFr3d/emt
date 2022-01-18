4402aadd5eb40809156544a29fe7d83f8bded77f
NIFr3d <fred-dls@orange.fr>
2022-01-18 10:59:13 +0100

    Update run.php

--- public_html/php/run.php
+++ public_html/php/run.php
@@ -2,6 +2,6 @@
 include("functions/BDD.php");
 $listeruns=$db->getRunHistory();
 $choix=$_GET["choix"];
-$choixstr=$listeruns[$choix];
+$choixstr=$listeruns[$choix][0];
 var_dump($db->getRunInfos($choixstr));
 ?>
