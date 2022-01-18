07bdb12ef021ca27b3eae2771c2ef9a2c825c53c
NIFr3d <fred-dls@orange.fr>
2022-01-18 11:16:41 +0100

    Update run.php

--- public_html/php/run.php
+++ public_html/php/run.php
@@ -7,11 +7,11 @@ $runinfos=$db->getRunInfos($choixstr);
 echo("<br /><br /><br /><TABLE BORDER='1' cellspacing='0'> 
       <TR><TH>Temps</TH><TH>Vitesse</TH><TH>Consommation</TH><TH>Latitude</TH><TH>Longitude</TH></TR>\n");
   for($i=0;$i<count($runinfos);$i++){
-      $temps=$liste[$i][0];
-      $vitesse=$liste[$i][1];
-      $consommation=$liste[$i][2];
-      $latitude=$liste[$i][3];
-      $longitude=$liste[$i][4];
+      $temps=$runinfos[$i][0];
+      $vitesse=$runinfos[$i][1];
+      $consommation=$runinfos[$i][2];
+      $latitude=$runinfos[$i][3];
+      $longitude=$runinfos[$i][4];
       echo("<TR><TD>$temps</TD><TD>$vitesse</TD><TD>$consommation</TD><TD>$latitude</TD>$longitude<TD></TD></TR>\n");
   }
   echo("</TABLE>\n");	
