3efc336b62d3ba6d3fbdde74be5a619498cfbf27
Lordiak <l.printz@laposte.net>
2022-01-21 16:26:02 +0000

    début afficher pos depuis histo

--- public_html/run.php
+++ public_html/run.php
@@ -5,7 +5,7 @@ $choix=$_GET["choix"];
 $choixstr=$listeruns[$choix][0];
 $runinfos=$db->getRunInfos($choixstr);
 echo("<br /><br /><br /><TABLE BORDER='1' cellspacing='0'> 
-      <TR><TH>Temps (s)</TH><TH>Vitesse (km/h)</TH><TH>Intensité (A)</TH><TH>Tension (V)</TH><TH>Energie (J)</TH><TH>Latitude (DD)</TH><TH>Longitude (DD)</TH></TR>\n");
+      <TR><TH>Temps (s)</TH><TH>Vitesse (km/h)</TH><TH>Intensité (A)</TH><TH>Tension (V)</TH><TH>Energie (J)</TH><TH>Latitude (DD)</TH><TH>Longitude (DD)</TH><TH>Aller à</TH></TR>\n");
   for($i=0;$i<count($runinfos);$i++){
       $temps=$runinfos[$i][0];
       $vitesse=$runinfos[$i][1];
@@ -14,7 +14,16 @@ echo("<br /><br /><br /><TABLE BORDER='1' cellspacing='0'>
       $energie=$runinfos[$i][4];
       $latitude=$runinfos[$i][5];
       $longitude=$runinfos[$i][6];
-      echo("<TR><TD>$temps</TD><TD>$vitesse</TD><TD>$intensite</TD><TD>$tension</TD><TD>$energie</TD><TD>$latitude</TD><TD>$longitude</TD></TR>\n");
+      echo("<TR>
+      <TD>$temps</TD>
+      <TD>$vitesse</TD>
+      <TD>$intensite</TD>
+      <TD>$tension</TD>
+      <TD>$energie</TD>
+      <TD>$latitude</TD>
+      <TD>$longitude</TD>
+      <TD><div id='aligniconegps'><form method='post' action='functions/afficherpos'><button id='boutongps'type='submit' name='iconegps'><img id='iconegps' src='styles/images/iconegps.png' /></button></form></div></TD>
+      </TR>\n");
   }
   echo("</TABLE>\n");	
 ?>
