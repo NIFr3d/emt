a768d294ba73a32b441377725845930068d7a5ed
NIFr3d <NIFr3d@github.com>
2022-02-08 21:19:02 +0000

    fbdfgbs gb

--- public_html/functions/changepassword.php
+++ public_html/functions/changepassword.php
@@ -5,7 +5,7 @@ $mdp=$_POST["mdp"];
 $mdpconfirm=$_POST["mdpconfirm"];
 if($mdp==$mdpconfirm){
     $db->changemdp(password_hash($mdp,PASSWORD_DEFAULT),$userid);
-    header("Location: /profil");
+    header("Location: /profil?e=Mot+de+passe+changé");
 }else{
     header("Location: /profil?e=Entrez+2+fois+le+même+mot+de+passe");
 }
