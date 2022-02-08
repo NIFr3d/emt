984b6e6d2d742b155ac6b8e02748a8a7de2571c1
L Raspberry <l.printz@laposte.net>
2022-02-08 16:33:12 +0000

    ff

---
+++ public_html/profil.php
@@ -0,0 +1,33 @@
+<html lang="fr">
+
+<head>
+    <title>
+        EMT 2021-2022
+    </title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../styles/main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon"/>
+    <div id="navbar"></div>
+    <script type="text/javascript">
+        $("#navbar").load("nav");
+    </script>
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
+
+</head>
+
+<body>
+    <div class="corps">
+    <?php
+    session_start();
+    if(!isset($_SESSION["acces"])) header("location: login");
+    ?>
+    
+    </div>
+</body>
+<footer>
+    <script src="../scripts/data.js"></script>
+</footer>
+</html>
+<script>
+</script>
