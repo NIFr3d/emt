6a98dec3b1ea3df39d64400fe8f3510d3cf5bf78
NIFr3d <NIFr3d@github.com>
2022-02-08 21:35:47 +0000

    etyhey

---
+++ public_html/cookiespolicy.php
@@ -0,0 +1,20 @@
+<html lang="fr">
+<head>
+    <title>
+        EMT 2021-2022
+    </title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../styles/main.css">
+    <link rel="shortcut icon" href="../favicon.ico" type="image/x-icon"/>
+    <div id="navbar"></div>
+</head>
+<body>
+    <div class="corps">
+    Ce site utilise des cookies afin de permettre une connexion plus rapide.
+    Conformément aux consignes du RGPD de l'Union Européenne, ces cookies sont considérés comme essentiels au fonctionnement du site et non désactivables.
+    </div>
+</body>
+<footer>
+    <script src="../scripts/nbuser.js"></script>
+</footer>
+</html>
