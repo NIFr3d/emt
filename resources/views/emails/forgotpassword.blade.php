6cb7833272080d5e4b1a5f13a7322fbe16f043c0
NIFr3d <fred-dls@orange.fr>
2022-02-26 18:27:23 +0100

    Update forgotpassword.blade.php

--- resources/views/emails/forgotpassword.blade.php
+++ resources/views/emails/forgotpassword.blade.php
@@ -1,10 +1,11 @@
 @component('mail::message')
 # Récupération de votre mot de passe
-
 Cliquez sur le lien ci-dessous pour récupérer votre mot de passe :
 
-@component('mail::button', ['url' => 'emt.univ-lorraine.fr'])
-Récupérer mon mot de passe
+(!!obligé de mettre https sinon google bloque le mail!!)
+
+@component('mail::button', ['url' => 'https://emt.univ-lorraine.fr'])
+Récupérer mon mot de passe 
 @endcomponent
 
 Cordialement,<br>
