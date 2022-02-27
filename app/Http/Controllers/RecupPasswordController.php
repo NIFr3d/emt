242e3037c1617f183d194ee44c24ca03dfc90ad4
NIFr3d <fred-dls@orange.fr>
2022-02-27 12:25:00 +0100

    Update RecupPasswordController.php

--- app/Http/Controllers/RecupPasswordController.php
+++ app/Http/Controllers/RecupPasswordController.php
@@ -11,8 +11,12 @@ use App\Mail\ForgotPassword;
 class RecupPasswordController extends Controller
 {
     function envoyerMail(){
-        $mail=$_POST["email"];
-        Mail::to($mail)->send(new ForgotPassword());
-        return redirect("/Connexion?erreur=Mail+envoyé");
+        $email=$_POST["email"];
+        $utilisateur=utilisateur::where("email",$email)->first();
+        if(!$utilisateur) return redirect("/Connexion?erreur=Mail+inconnu");
+        else {
+            Mail::to($email)->send(new ForgotPassword());
+            return redirect("/Connexion?erreur=Mail+envoyé");
+        }
     }
 }
