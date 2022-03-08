3e9756e037b7dbc1564379eccf5242856efb0479
Lordiak <l.printz@laposte.net>
2022-03-08 15:45:03 +0000

    fix excel

--- public_html/functions/download.php
+++ public_html/functions/download.php
@@ -16,14 +16,21 @@ $listeruns=$db->getRunHistory();
 $choix=$_POST["choix"];
 $choixstr=$listeruns[$choix][0];
 $runinfos=$db->getRunInfos($choixstr);
+$s->setCellValueByColumnAndRow(1, 1, "temps");
+$s->setCellValueByColumnAndRow(2, 1, "vitesse");
+$s->setCellValueByColumnAndRow(3, 1, "intensité");
+$s->setCellValueByColumnAndRow(4, 1, "tension");
+$s->setCellValueByColumnAndRow(5, 1, "énergie");
+$s->setCellValueByColumnAndRow(6, 1, "latitude");
+$s->setCellValueByColumnAndRow(7, 1, "longitude");
 for($i=1;$i<count($runinfos)+1;$i++){
-    $s->setCellValueByColumnAndRow(1, $i, $runinfos[$i-1][0]);
-    $s->setCellValueByColumnAndRow(2, $i, $runinfos[$i-1][1]);
-    $s->setCellValueByColumnAndRow(3, $i, $runinfos[$i-1][2]);
-    $s->setCellValueByColumnAndRow(4, $i, $runinfos[$i-1][3]);
-    $s->setCellValueByColumnAndRow(5, $i, $runinfos[$i-1][4]);
-    $s->setCellValueByColumnAndRow(6, $i, $runinfos[$i-1][5]);
-    $s->setCellValueByColumnAndRow(7, $i, $runinfos[$i-1][6]);
+    $s->setCellValueByColumnAndRow(1, $i+1, $runinfos[$i-1][0]);
+    $s->setCellValueByColumnAndRow(2, $i+1, $runinfos[$i-1][1]);
+    $s->setCellValueByColumnAndRow(3, $i+1, $runinfos[$i-1][2]);
+    $s->setCellValueByColumnAndRow(4, $i+1, $runinfos[$i-1][3]);
+    $s->setCellValueByColumnAndRow(5, $i+1, $runinfos[$i-1][4]);
+    $s->setCellValueByColumnAndRow(6, $i+1, $runinfos[$i-1][5]);
+    $s->setCellValueByColumnAndRow(7, $i+1, $runinfos[$i-1][6]);
     
 }
 
